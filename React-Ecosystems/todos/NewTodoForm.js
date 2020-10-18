efa9f536cd02ed49e0f0f6df159f2075888cd36b
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-10-17 20:42:05 -0700

    added statetoprops and dispatchtoprops functions

--- React-Ecosystems/todos/NewTodoForm.js
+++ React-Ecosystems/todos/NewTodoForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createToDo } from './actions';
 // import './NewTodoForm.css';
 
-const NewTodoForm = () => {
+const NewTodoForm = ({ todos, onCreatePressed }) => {
    const [inputValue, setInputValue] = useState('');
 
    return (
@@ -12,9 +14,26 @@ const NewTodoForm = () => {
         placeholder="Type your new to-do here"
         value={inputValue}
         onChange={e => setInputValue(e.target.value)}></input>
-        <button className="new-todo-btn"></button>
+        <button 
+        className="new-todo-btn"
+        onClick={() => {
+            const isDuplicateText =
+                todos.some(todo => todo.text === inputValue);
+            if (!isDuplicateText) {
+                onCreatePressed(inputValue);
+            setInputValue('');
+            }
+        }}
+        ></button>
     </div>
    );
 }
 
-export default NewTodoForm;
+const mapStateToProps = state => ({
+    todos: state.todos,
+});
+const mapDispatchToProps = dispatch => ({
+    onCreatePressed: text => dispatch(createTodo(text))
+});
+
+export default connect()(NewTodoForm);
